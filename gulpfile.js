3b60de7e99c57b79a534b1851c23185892160849
Huy Nguyen <huynguyen.me@gmail.com>
2018-06-04 00:59:39 -0700

    First Init
    Tailwind Boilerplate with Gulp

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var tailwindcss = require('tailwindcss');
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('start', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("./styles/scss/*.scss", ['sass']);
+    gulp.watch("./styles/css/*.css", ['tailwind', browserSync.reload]);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./styles/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./styles/css'));
+});
+
+gulp.task('tailwind', function() {
+    return gulp.src('./styles/css/main.css')
+        .pipe(postcss([
+            tailwindcss('./tailwind.js'),
+            autoprefixer,
+        ]))
+        .pipe(gulp.dest('./styles/public/css/'))
+})
+
+gulp.task('default', ['start']);
