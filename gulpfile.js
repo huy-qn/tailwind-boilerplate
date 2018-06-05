9ec24d673f0410ea7094665cc176cc7b4b563338
Huy Nguyen <huynguyen.me@gmail.com>
2018-06-04 19:13:50 -0700

    Handle error

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync').create();
 
 // Static server
-gulp.task('start', function() {
+gulp.task('start', function () {
     browserSync.init({
         server: {
             baseDir: "./"
@@ -23,16 +23,24 @@ gulp.task('start', function() {
 gulp.task('sass', function () {
     return gulp.src('./styles/scss/*.scss')
         .pipe(sass())
+        .on('error', swallowError)
         .pipe(gulp.dest('./styles/css'));
 });
 
-gulp.task('tailwind', function() {
+gulp.task('tailwind', function () {
     return gulp.src('./styles/css/main.css')
         .pipe(postcss([
             tailwindcss('./tailwind.js'),
             autoprefixer,
         ]))
+        .on('error', swallowError)
         .pipe(gulp.dest('./styles/public/css/'))
 })
 
 gulp.task('default', ['start']);
+
+function swallowError(error) {
+    // If you want details of the error in the console
+    console.log(error.toString())
+    this.emit('end')
+}
